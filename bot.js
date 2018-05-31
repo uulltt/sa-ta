3766c603e41ae43a2c7418c5c93afc1145413d9e
uulltt <ajvark@gmail.com>
2018-05-31 14:38:35 -0400

    idk anymore

--- bot.js
+++ bot.js
@@ -4,6 +4,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
 	console.log('I am ready!');
+	console.log("AAAAAAAA");
 	client.user.setUsername("SA-TA");
 	client.user.setActivity('type !help for commands', {
 		type: 'WATCHING'
@@ -12,13 +13,13 @@ client.on('ready', () => {
 
 
 client.on('message', message => {
-	if (message.cleanContent.startsWith('!ready ')){
+	if (message.content.startsWith('!ready ')){
 		console.log("AAAAAAAA");
 		var role = message.guild.roles.find("Looking for Game");
 		console.log(role);
 		message.member.addRole(role).then(console.log).catch(console.error);
 	}
-	if (message.cleanContent.startsWith('!unready ')){
+	if (message.content.startsWith('!unready ')){
 		var role = message.guild.roles.find("Looking for Game");
 		message.member.removeRole(role).then(console.log).catch(console.error);
 	}
