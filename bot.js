eed44189b0cfedc7c6203cf8cc909c495bb2980f
uulltt <ajvark@gmail.com>
2018-05-31 14:11:48 -0400

    fixing bot stuff yeah

--- bot.js
+++ bot.js
@@ -13,8 +13,17 @@ client.on('ready', () => {
 
 
 client.on('message', message => {
-	
+	if (message.cleanContent.startsWith('!ready '){
+		let role = message.guild.roles.find("Looking for Game");
+		message.member.addRole(role).catch(console.error);
+	}
+	if (message.cleanContent.startsWith('!unready '){
+		let role = message.guild.roles.find("Looking for Game");
+		message.member.removeRole(role).catch(console.error);
+	}
+	/*if (message.cleanContent.startsWith('!whosready '){
 		
+	}*/	
 		
 	});
 
