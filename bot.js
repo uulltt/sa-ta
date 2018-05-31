1d69a6e091c836484ff3d867e3d856b6de10d0d2
uulltt <ajvark@gmail.com>
2018-05-31 15:32:26 -0400

    adding stuff

--- bot.js
+++ bot.js
@@ -10,6 +10,91 @@ client.on('ready', () => {
 	});
 });
 
+const ln = [
+    "Let's Make A Comet!",
+    "Dwarf Stars",
+    "Hop And Skip",
+    "Pipe Dreams",
+    "Lakeside",
+    "Four Corners",
+    "Prickly Meadow",
+    "Rise and Fall",
+    "Simply Stumped",
+    "Cut Them Down",
+    "Cluttered Copse",
+    "Buggy Forest",
+    "Beware the Bouncy Mushrooms",
+    "Sticky Switches",
+    "Skillepede",
+    "Backwoods at Bedtime",
+    "Hopping Beach",
+    "Capsized Cannons",
+    "Crabbytown Capers",
+    "Water Wickets",
+    "Sliding on the Sand",
+    "Oil Rigged",
+    "Catkus Resort",
+    "Floating Above Clouds",
+    "Push It, Curve It",
+    "Mellow Meadow",
+    "Barricade Crusade",
+    "Bounce Bastion",
+    "Lights Out",
+    "Crash Course",
+    "Bounding About Bee Block Base",
+    "Doze Off, Press On",
+    "Debris Dodger",
+    "Dragonslayer",
+    "Big Bangs",
+    "Blazing Hot Sand",
+    "Hot Rod Rocket",
+    "Golf Balls and Fire Walls",
+    "Mirage A Tre",
+    "Mt. Huffinpuff",
+    "Slippery Slope",
+    "Cemetary Crawl",
+    "Haunt the Halls",
+    "Crystaline Incline",
+    "Diamond Derby",
+    "Lightning Frightening",
+    "Deep Sleep Dungeon Delve",
+    "Mystery Missile",
+    "Enemy Engineering",
+    "Buckle Up for Belt Rides",
+    "Freeze to Float",
+    "Pipe Nightmare",
+    "Time to Stop",
+    "The Great Escape",
+    "Battlefield Blitz",
+    "Screws Loose in Mad Machines",
+    "Outer Odyssey",
+    "Lightspeed",
+    "Bug Zapper",
+    "Prism Prison",
+    "Fireflies Light Up the Night",
+    "Space Base Warp Trace",
+    "Bugs in the System",
+    "Shooting Star"
+];
+
+const ch = [
+    "One Shot Wonder",
+    "Release the Beast",
+    "Autumn Forest Slide Ball",
+    "Let Me In!",
+    "Six-Footed Race",
+    "Autoball Alcove",
+    "Metal Bounce",
+    "Beehive Beetdown",
+    "Fiery Fury Hurry",
+    "Snow Slider"
+    "Skipping Stone",
+    "My Buggy Buddy",
+    "Smashing The Ice",
+    "A Little Push, A Little Nudge",
+    "Flight to the Finish"
+];
+
 
 client.on('message', message => {
 	if (message.content.substring(0, 6) === "!ready"){
@@ -32,6 +117,31 @@ client.on('message', message => {
 		} else {
 			message.channel.send("Error. There is no \"Looking for Game\" role.");
 		}
+	}
+	
+	if (message.content.startsWith("!stats ")){
+		var level = message.content.substring(7);
+			var request = require('request').defaults({
+encoding: null
+		});
+		request.get(encodeURI(' http://rightstickstudios.com/wickets/api/v1/stats.php?id=' + level.replace(/ /gm, '')), function (err, res, body) {
+		var data = JSON.parse(body.toString()).data;
+		console.log(data);
+		var title = '. ';
+		if (level.toLowerCase().charAt(0) === 'l'){
+			title = ln[parseInt(level.substring(1))];
+		}
+		if (level.toLowerCase().charAt(0) === 'c'){
+			title = ch[parseInt(level.substring(1))];
+		}
+		const embed = new Discord.RichEmbed().setTitle(title);
+		for (var propName in data) {
+			propValue = data[propName];
+			embed.addField(propName.toString(), propValue.toString());
+		}
+		message.channel.send(embed);
+		});
+		
 	}
 		
 	});
