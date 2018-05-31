28e8d80850489937c6c7d26e9ab5086d0368afb9
uulltt <ajvark@gmail.com>
2018-05-31 15:07:34 -0400

    this might be it

--- bot.js
+++ bot.js
@@ -13,18 +13,25 @@ client.on('ready', () => {
 
 client.on('message', message => {
 	if (message.content.substring(0, 6) === "!ready"){
-		console.log("AAAAAAAA");
-		console.log(message.guild.roles);
 		let role = message.guild.roles.array().filter(function(item) {
 		return item.name === "Looking for Game";	
 		});
-		console.log(role);
-		//message.member.addRole(role).then(message.channel.send("Alright <@"+message.author.id+">, you're looking for a game!")).catch(console.error);
+		if (role.length > 0){
+		message.member.addRole(role[0]).then(message.channel.send("Alright <@"+message.author.id+">, you're looking for a game!")).catch(console.error);
+		} else {
+			message.channel.send("Error. There is no \"Looking for Game\" role.");
+		}
 		
 	}
 	if (message.content.substring(0, 8) === "!unready"){
-		let role = message.guild.roles
-		message.member.removeRole(role).then(message.channel.send("Alright <@"+message.author.id+">, you're no longer looking for a game!")).catch(console.error);
+		let role = message.guild.roles.array().filter(function(item) {
+		return item.name === "Looking for Game";	
+		});
+		if (role.length > 0){
+		message.member.removeRole(role[0]).then(message.channel.send("Alright <@"+message.author.id+">, you're no longer looking for a game!")).catch(console.error);
+		} else {
+			message.channel.send("Error. There is no \"Looking for Game\" role.");
+		}
 	}
 		
 	});
