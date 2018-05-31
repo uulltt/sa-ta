6796d52f56f77d245b5e1afd6435c8e1645d9079
uulltt <ajvark@gmail.com>
2018-05-31 15:05:07 -0400

    testing out a thing hold on

--- bot.js
+++ bot.js
@@ -15,13 +15,15 @@ client.on('message', message => {
 	if (message.content.substring(0, 6) === "!ready"){
 		console.log("AAAAAAAA");
 		console.log(message.guild.roles);
-		//let role = message.guild.roles.find("Looking for Game");
-		//console.log(role);
+		let role = message.guild.roles.array().filter(function(item) {
+		return item.name === "Looking for Game";	
+		});
+		console.log(role);
 		//message.member.addRole(role).then(message.channel.send("Alright <@"+message.author.id+">, you're looking for a game!")).catch(console.error);
 		
 	}
 	if (message.content.substring(0, 8) === "!unready"){
-		let role = message.guild.roles.find("Looking for Game");
+		let role = message.guild.roles
 		message.member.removeRole(role).then(message.channel.send("Alright <@"+message.author.id+">, you're no longer looking for a game!")).catch(console.error);
 	}
 		
