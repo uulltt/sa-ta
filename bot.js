2b9a1587ff2b79b7b9862271f6bdf7520efc57f0
uulltt <ajvark@gmail.com>
2018-05-31 14:55:48 -0400

    fixing more stuff

--- bot.js
+++ bot.js
@@ -13,15 +13,16 @@ client.on('ready', () => {
 
 
 client.on('message', message => {
-	if (message.content.startsWith('!ready ')){
+	if (message.content.startsWith('!ready')){
 		console.log("AAAAAAAA");
 		var role = message.guild.roles.find("Looking for Game");
 		console.log(role);
-		message.member.addRole(role).then(console.log).catch(console.error);
+		message.member.addRole(role).then(message.channel.send("Alright <@"+message.author.id+">, you're looking for a game!")).catch(console.error);
+		
 	}
-	if (message.content.startsWith('!unready ')){
+	if (message.content.startsWith('!unready')){
 		var role = message.guild.roles.find("Looking for Game");
-		message.member.removeRole(role).then(console.log).catch(console.error);
+		message.member.removeRole(role).then(message.channel.send("Alright <@"+message.author.id+">, you're no longer looking for a game!")).catch(console.error);
 	}
 	/*if (message.cleanContent.startsWith('!whosready '){
 		
